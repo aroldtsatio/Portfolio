14e370b01dfdd73327eef3665a0ed0ec8e3149b7
Arold <aroldtsatio@gmail.com>
2025-04-01 23:01:53 +0200

    Installation Projet React and Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  console.log("App.jsx");
+
+  return (
+   <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>  
+  );
+}
+
+export default App;
